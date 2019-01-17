7c9a94356cf1efafec27f3e9f2ee93a64c17fb3d
thisnameisprivate <2632798785@qq.com>
2019-01-17 08:33:23 +0800

    save code.

--- ReflectionMethod.php
+++ ReflectionMethod.php
@@ -2,6 +2,7 @@
 
 // 使用 ReflectionClass::getMethods() 来获取对象的所有方法并用ReflectionMethod来检查
 
+/*
 $prod_class = new ReflectionClass('CdProduct');
 $methods = $prod_class->getMethods();
 foreach ($methods as $method) {
@@ -39,4 +40,39 @@ function methodData (ReflectionMethod $method) {
         $details .= "$name returns a reference (as opposed to a value)\n";
     }
     return $details;
+}
+
+  */
+/*
+$con_config = [
+    'user' => 'root',
+    'pwd'  => '',
+    'db'   => 'visit',
+    'host' => 'localhost'
+];
+$conn = new PDO("mysql:host=" . $con_config['localhost'] . ';dbname=' . $con_config['db'], $con_config['user'], $con_config['pwd']);
+$conn->query("set names 'utf8'");
+$sql = "SELECT * FROM alldiseases WHERE id = ?";
+$bindValue = 2;
+$stmt = $conn->prepare($sql);
+$stmt->bindValue(1, $bindValue);
+$stmt->execute();
+while ($row_result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    print_r($row_result);
+}
+
+ */
+
+header("Content-type:text/html; charset=utf-8");
+$mysql = new mysqli("localhost", 'root', '', 'visit');
+$mysql->query("SET NAMES UTF8");
+$result = $mysql->query("SELECT * FROM alldiseases");
+while ($row = $result->fetch_assoc()) {
+    $district[$row['id']] = ['id' => $row['id'], 'pid' => $row['pid'], 'name' => $row['name']];
+}
+function arrayToTree (Array $items) {
+    foreach ($items as $item) {
+        $item[$item['pid']]['son'][$item['id']] = &$items[$item['id']];
+    }
+    return isset($items[0]['son']) ? $items[0]['son'] : [];
 }
