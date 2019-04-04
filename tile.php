fba9e7e57a3f296090aa9cdb90c9d90dc090202b
thisnameisprivate <2632798785@qq.com>
2019-04-04 09:52:33 +0800

    new code save

---
+++ tile.php
@@ -0,0 +1,28 @@
+<?php
+
+
+abstract class Tile {
+    abstract function getWealthFactor ();
+}
+class Plains extends Tile {
+    private $wealthfactor = 2;
+    function getWealthFactor () {
+        return $this->wealthfactor;
+    }
+}
+abstract class TileDecorator extends Tile {
+    protected $tile;
+    function __construct (Tile $tile) {
+        $this->tile = $tile;
+    }
+}
+class DiamondDecorator extends TileDecorator {
+    public function getWealthFactor () {
+        return $this->tile->getWealthFactor() + 2;
+    }
+}
+class PollutionDecorator extends TileDecorator {
+    public function getWealthFactor () {
+        return $this->tile->getWealthFactor() - 4;
+    }
+}
