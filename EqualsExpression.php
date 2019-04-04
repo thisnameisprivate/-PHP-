a849c14a2b0850282aaad8476bd6e0d6b8148fc5
thisnameisprivate <2632798785@qq.com>
2019-04-04 13:13:53 +0800

    new code save

---
+++ EqualsExpression.php
@@ -0,0 +1,12 @@
+<?php
+
+class EqualsExpression extends OperatorExpression {
+    protected function doInterpret (InterpreterContext $context, $result_l, $result_r) {
+        $context->replace($this, $result_l || $result_r);
+    }
+}
+class BooleanAndExpression extends OperatorExpression {
+    protected function doInterpret (InterpreterContext $context, $result_l, $result_r) {
+        $context->replace($this, $result_l && $result_r);
+    }
+}
