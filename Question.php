1dea913561a2050f7baa7d87763ff23577cd723e
thisnameisprivate <2632798785@qq.com>
2019-04-07 10:53:22 +0800

    new code save

--- Question.php
+++ Question.php
@@ -19,3 +19,32 @@ class TextQuestion extends Question {
 class AVQuestion extends Question {
     // 处理语音问题特有操作
 }
+abstract class Marker {
+    protected $test;
+    public function __construct ($test) {
+        $this->test = $test;
+    }
+    abstract function mark ($response);
+}
+class MarkLogicMarker extends Marker {
+    private $engine;
+    public function __construct ($test) {
+        parent::__construct ($test);
+    }
+    public function mark ($response) {
+        // return $this->engine->evaluate($response);
+        return true;
+    }
+}
+class MatchMarker extends Marker {
+    public function mark ($response) {
+        return ($this->test == $response);
+    }
+}
+class RegexpMarker extends Marker {
+    public function mark ($response) {
+        return (preg_match($this->test, $response));
+    }
+}
+
+$markers = array(new RegexpMarker("/f.ve/"), new MatchMarker("five"), new MarkLogicMarker("$input equals 'five'"));
