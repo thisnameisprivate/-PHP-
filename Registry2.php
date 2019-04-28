dac868287acbee20f0f4ac8e9cd0aede5d2a21a2
thisnameisprivate <2632798785@qq.com>
2019-04-28 09:55:23 +0800

    new code save

--- Registry2.php
+++ Registry2.php
@@ -7,7 +7,27 @@ class Registry2 {
 
     }
     public static function instance () {
-        if (! isset(sefl::$instasnce)) return self::$instance = new Registry2();
+        if (! isset(self::$instasnce)) return self::$instance = new Registry2();
+    }
+    public function get ($key) {
+        if (isset($this->values[$key])) {
+            return $this->values[$key];
+        }
+        return null;
+    }
+    public function set ($key, $value) {
+        $this->values[$key] = $value;
+    }
+}
+
+class Registry3 {
+    private static $instance;
+    private $values = [];
+    private function __construct () {
+
+    }
+    public static function instance () {
+        if (! isset(self::$instance)) return self::$instance = new Registry3();
     }
     public function get ($key) {
         if (isset($this->values[$key])) {
