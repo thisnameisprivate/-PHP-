d55174740ca66e42c55f600e7ceb3920d5e9d83b
thisnameisprivate <2632798785@qq.com>
2019-05-03 09:48:50 +0800

    随便写的JS

--- ToPrimitive.js
+++ ToPrimitive.js
@@ -1,1 +1,9 @@
-var o = {};
+if (document.getElementById('IdenController-item')) {
+    console.log("Hello, world.");
+}
+
+var item = document.getElementsByTagName('item-0')[0];
+item.addEventListener('click', () => {
+    console.log(":console.log('JavaScript;')");
+})
+item.removeEventListener('click');
